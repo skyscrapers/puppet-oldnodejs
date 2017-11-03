0d0ab566075402426777de96d73bae2c3f1b1ee7
Luca Venturelli <luca@skyscrapers.eu>
2017-11-03 17:55:38 +0100

    fixing repo config

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -71,6 +71,7 @@ class oldnodejs::repo {
       release  => $::lsbdistcodename,
       repos    => 'main',
       key      => {
+        'id'     => '9FD3B784BC1C6FC31A8A0A1C1655A0AB68576280',
         'source' => 'https://deb.nodesource.com/gpgkey/nodesource.gpg.key',
       },
     }
